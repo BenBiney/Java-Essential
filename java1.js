9d57ff9d53cec1d636b2e9844117bb0c7eba779c
BenBiney <72565324+BenBiney@users.noreply.github.com>
2020-10-08 17:05:12 +0100

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,13 @@
+
+
+let kwesi=document.getElementsByName(css);
+let mensa=document.getElementById(alter);
+function benjamin()
+{
+    for(let i=0;i<kwesi.length;i++){
+    let css = kwesi[one].getAttribute('id');
+    let Xy=kwesi[one].value;
+    mensa.style[css]=Xy
+}
+}
+document.getElementById('Apply').addEventListener('click Apply')
