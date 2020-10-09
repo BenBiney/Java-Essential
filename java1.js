077f4df966df6ef3bdb24d006678274155bc5628
BenBiney <72565324+BenBiney@users.noreply.github.com>
2020-10-09 10:49:56 +0100

    Update java1.js

--- java1.js
+++ java1.js
@@ -1,8 +1,8 @@
 
 
 let kwesi=document.getElementsByName(css);
-let mensa=document.getElementById(alter);
-function benjamin()
+let mensa=document.getElementById(apply);
+function apply()
 {
     for(let i=0;i<kwesi.length;i++){
     let css = kwesi[one].getAttribute('id');
